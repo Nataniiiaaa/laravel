6886a914f7fd5b353e79cecbcc4edc76d46b9ada
Natania Setyawati <natania.nn64@gmail.com>
2023-11-25 15:13:42 +0800

    vue progres

--- frontend-app/src/components/Form.vue
+++ frontend-app/src/components/Form.vue
@@ -1,26 +1,39 @@
 <template>
-    <div>
-    Test 12345
-    </div>
+    <div>Test 12345</div>
 </template>
 
 <script>
-    import axios from 'redaxios'
+import axios from "redaxios";
 
-    export default {
-        name : 'form',
-        data(){
-            return{
-                allForm : {},
-                form:{
-                    'id':'',
-                    'nama':'',
-                    'alamat':'',
-                    'telepon':'',
-                    'gender':''
-                }
-            }
-
-        }
-    }
+export default {
+    name: "form",
+    data() {
+        return {
+            allform: {},
+            form: {
+                id: "",
+                nama: "",
+                alamat: "",
+                telepon: "",
+                gender: "",
+            },
+        };
+    },
+    created() {
+        console.log("Created !");
+        this.loadAllForm();
+    },
+    mounted() {
+        console.log("Mounted");
+    },
+    methods: {
+        loadAllForm() {
+            var url = `http://127.0.0.1:8000/api/form`;
+            axios.get(url).then();
+            ({ data }) => {
+                console.log(data);
+            };
+        },
+    },
+};
 </script>
