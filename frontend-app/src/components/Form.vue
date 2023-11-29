8f504f60e2c996228c492b71c18eaf133bf81acb
Natania Setyawati <natania.nn64@gmail.com>
2023-11-29 13:25:03 +0800

    progress

--- frontend-app/src/components/Form.vue
+++ frontend-app/src/components/Form.vue
@@ -1,5 +1,29 @@
 <template>
-    <div>Test 12345</div>
+    <div class="container">
+        <h2>Data Form</h2>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Nama</td>
+                    <td>Alamat</td>
+                    <td>Telepon</td>
+                    <td>Gender</td>
+                    <td>Action</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(form, index) in allform" :key="form.id">
+                    <td>{{ form.id }}</td>
+                    <td>{{ form.nama }}</td>
+                    <td>{{ form.alamat }}</td>
+                    <td>{{ form.telepon }}</td>
+                    <td>{{ form.gender }}</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
